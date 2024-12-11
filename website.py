11d372eeca77f6254531fac45fc609da37150f40
Andrew Jowe <andrewjowe@yahoo.com>
2024-12-10 20:13:15 -0800

    fin

--- website.py
+++ website.py
@@ -2,11 +2,12 @@ from frontend import app
 from train_save_model import train_and_save_model
 
 TRAIN_REQUIRED = False # change to True when running for the first time
-DEBUG = True # change to True when debugging
+DEBUG = False # change to True when debugging
+PORT = 5000
 
 if __name__ == "__main__":
     if TRAIN_REQUIRED:
         # Train and save model
         train_and_save_model(movie_data_path="data/cleaned_data.csv", user_data_path="data/user_data.csv")
 
-    app.run(debug=DEBUG)
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
